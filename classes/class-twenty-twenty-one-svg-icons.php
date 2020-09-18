ba8dbfe8e04c65e75b3761c578eb3dc7880a2cf5
William Patton <will@pattonwebz.com>
2020-09-18 22:40:58 +0100

    Update DocBlock in class-twenty-twenty-one-svg-icons.php to make more sense

--- classes/class-twenty-twenty-one-svg-icons.php
+++ classes/class-twenty-twenty-one-svg-icons.php
@@ -10,8 +10,8 @@
 /**
  * This class is in charge of displaying SVG icons across the site.
  *
- * Place each <svg> source on its own array key, without adding the
- * both `width` and `height` attributes, since these are added dnamically,
+ * Place each <svg> source on its own array key, without adding either
+ * the `width` or `height` attributes, since these are added dynamically,
  * before rendering the SVG code.
  *
  * All icons are assumed to have equal width and height, hence the option
