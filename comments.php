778f06405fb7c74dcd0fd99f7408443b7653841f
Kelly Dwan <ryelle@users.noreply.github.com>
2020-09-25 18:54:04 -0400

    Remove unused translation comment & title

--- comments.php
+++ comments.php
@@ -35,10 +35,9 @@ if ( post_password_required() ) {
 				esc_html_e( '1 Reply', 'twentytwentyone' );
 			} else {
 				printf(
-					/* Translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s Reply', '%1$s Replies', $twenty_twenty_one_comment_count, 'comments title', 'twentytwentyone' ) ),
-					esc_html( number_format_i18n( $twenty_twenty_one_comment_count ) ),
-					'<span>' . get_the_title() . '</span>' // phpcs:ignore WordPress.Security.EscapeOutput
+					/* Translators: %s: comment count number. */
+					esc_html( _nx( '%s Reply', '%s Replies', $twenty_twenty_one_comment_count, 'comments title', 'twentytwentyone' ) ),
+					esc_html( number_format_i18n( $twenty_twenty_one_comment_count ) )
 				);
 			}
 			?>
