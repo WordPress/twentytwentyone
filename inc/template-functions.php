4b384681d1b235d0878a965fab7aad5ea468c954
Niels Lange <info@nielslange.de>
2020-10-01 20:01:43 +0700

    🐛 FIX: Adjust READ MORE button (#206)

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -162,7 +162,7 @@ function twenty_twenty_one_continue_reading_link() {
 			the_title( '<span class="screen-reader-text">' . __( 'about ', 'twentytwentyone' ), '</span>', false )
 		);
 
-		return '&hellip; <a class="more-link" href="' . esc_url( get_permalink() ) . '">' . $continue_reading . '</a>';
+		return '<a class="more-link" href="' . esc_url( get_permalink() ) . '">' . $continue_reading . '</a>';
 	}
 }
 
