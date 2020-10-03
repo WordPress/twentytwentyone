80535bd87916656b4d54851ec7ec3db35a1d091c
Jessica Lyschik <luminuu@gmail.com>
2020-10-03 19:54:18 +0200

    Fix serach results page, removing home.php and template-parts/content/blog.php

--- template-parts/content/content-excerpt.php
+++ template-parts/content/content-excerpt.php
@@ -15,10 +15,9 @@
 	<header class="entry-header">
 		<?php
 		the_title( sprintf( '<h2 class="entry-title default-max-width"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		twenty_twenty_one_post_thumbnail();
 		?>
 	</header><!-- .entry-header -->
-
-	<?php twenty_twenty_one_post_thumbnail(); ?>
 
 	<div class="entry-content">
 		<?php the_excerpt(); ?>
