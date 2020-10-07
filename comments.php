eddd4fb91379a2a0ee86481becdc833ac934c4fe
Glauber <glaubernm@gmail.com>
2020-10-07 13:29:03 -0300

    conform to phpcs wordpress standards

--- comments.php
+++ comments.php
@@ -84,8 +84,8 @@ if ( post_password_required() ) {
 
 	comment_form(
 		array(
-			'logged_in_as' => null,
-			'title_reply'  => esc_html__( 'Leave a reply', 'twentytwentyone' ),
+			'logged_in_as'       => null,
+			'title_reply'        => esc_html__( 'Leave a reply', 'twentytwentyone' ),
 			'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
 			'title_reply_after'  => '</h2>',
 		)
