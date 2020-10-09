1efbbe15be18b881b236203a1e3bb5db260dd849
Ari Stathopoulos <aristath@gmail.com>
2020-10-09 16:15:55 +0300

    Hide title on aside & status

--- template-parts/header/excerpt-header.php
+++ template-parts/header/excerpt-header.php
@@ -7,6 +7,12 @@
  * @since 1.0.0
  */
 
+// Don't show the title if the post-format is `aside` or `status`.
+$post_format = get_post_format();
+if ( 'aside' === $post_format || 'status' === $post_format ) {
+	return;
+}
+
 ?>
 
 <header class="entry-header">
