b8dde10e55a6af277013c5489661578c0786169a
Kelly Dwan <ryelle@users.noreply.github.com>
2020-10-12 16:45:40 -0400

    Align equals for phpcs

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -7,9 +7,9 @@
  * @since 1.0.0
  */
 
-$blog_info   = get_bloginfo( 'name' );
-$description = get_bloginfo( 'description', 'display' );
-$show_title = ( true === get_theme_mod( 'display_title_and_tagline', true ) );
+$blog_info    = get_bloginfo( 'name' );
+$description  = get_bloginfo( 'description', 'display' );
+$show_title   = ( true === get_theme_mod( 'display_title_and_tagline', true ) );
 $header_class = $show_title ? 'site-title' : 'screen-reader-text';
 
 ?>
