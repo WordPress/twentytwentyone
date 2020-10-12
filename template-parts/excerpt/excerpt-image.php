e6cea372a0f92b45cb8e692a54951064e628a3b9
Ari Stathopoulos <aristath@gmail.com>
2020-10-12 12:30:27 +0300

    Another fallback for images

--- template-parts/excerpt/excerpt-image.php
+++ template-parts/excerpt/excerpt-image.php
@@ -9,10 +9,21 @@
  * @since 1.0.0
  */
 
+$image_was_printed = true;
+
 // If there is no featured-image print the first image block we can find.
 if ( ! twenty_twenty_one_can_show_post_thumbnail() ) {
-	twenty_twenty_one_print_first_instance_of_block( 'core/image', get_the_content() );
+	if ( ! twenty_twenty_one_print_first_instance_of_block( 'core/image', get_the_content() ) ) {
+		$image_was_printed = false;
+	}
 }
 
-// Add the excerpt.
-the_excerpt();
+if ( $image_was_printed ) {
+
+	// Add the excerpt.
+	the_excerpt();
+} else {
+
+	// Fallback to the content.
+	the_content();
+}
