bb3755af4773f5a248e8afa32fb7f592478f63e9
Ari Stathopoulos <aristath@gmail.com>
2020-10-12 10:20:04 +0300

    audio

---
+++ template-parts/excerpt/excerpt-audio.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Show the excerpt.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since 1.0.0
+ */
+
+// Print the 1st instance of an audio block.
+if ( ! twenty_twenty_one_print_first_instance_of_block( 'core/audio', get_the_content() ) ) {
+	// If there was no audioblock, print the 1st audio-embed block.
+	twenty_twenty_one_print_first_instance_of_block( 'core/embed', get_the_content() );
+}
+
+// Add the excerpt.
+the_excerpt();
