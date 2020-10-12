a0f283f63e5140f888df0f020d54610f617416d2
Ari Stathopoulos <aristath@gmail.com>
2020-10-12 12:44:53 +0300

    improve video

--- template-parts/excerpt/excerpt-video.php
+++ template-parts/excerpt/excerpt-video.php
@@ -10,12 +10,15 @@
  */
 
 // Print the 1st video we can find.
-if ( twenty_twenty_one_print_first_instance_of_block( 'core/embed', get_the_content() ) ) {
-
-	// Add the excerpt.
-	the_excerpt();
-} else {
+if (
+	! twenty_twenty_one_print_first_instance_of_block( 'core/video', get_the_content() ) &&
+	! twenty_twenty_one_print_first_instance_of_block( 'core/embed', get_the_content() )
+) {
 
 	// Fallback to the content if no block was found.
 	the_content();
+} else {
+
+	// Add the excerpt.
+	the_excerpt();
 }
