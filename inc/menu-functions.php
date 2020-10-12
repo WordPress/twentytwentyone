24f935804fb8042b528d81e83d40e66c699a499a
Ari Stathopoulos <aristath@gmail.com>
2020-10-12 18:20:58 +0300

    See #414

--- inc/menu-functions.php
+++ inc/menu-functions.php
@@ -26,7 +26,7 @@
  * @return string Nav menu item start element.
  */
 function twenty_twenty_one_add_sub_menu_toggle( $output, $item, $depth, $args ) {
-
+	error_log( print_r( $item, true ) );
 	if ( 0 === $depth && in_array( 'menu-item-has-children', $item->classes, true ) ) {
 
 		// Add toggle button.
@@ -35,3 +35,35 @@ function twenty_twenty_one_add_sub_menu_toggle( $output, $item, $depth, $args ) 
 	return $output;
 }
 add_filter( 'walker_nav_menu_start_el', 'twenty_twenty_one_add_sub_menu_toggle', 10, 4 );
+
+/**
+ * Filters a menu item's title.
+ *
+ * @since 4.4.0
+ *
+ * @param string   $title The menu item's title.
+ * @param WP_Post  $item  The current menu item.
+ * @param stdClass $args  An object of wp_nav_menu() arguments.
+ * @param int      $depth Depth of menu item. Used for padding.
+ */
+function twenty_twenty_one_add_menu_description( $title, $item, $args, $depth ) {
+	return $title;
+}
+add_filter( 'nav_menu_item_title', 'twenty_twenty_one_add_menu_description', 10, 4 );
+
+/**
+ * Filters the arguments for a single nav menu item.
+ *
+ * @since 4.4.0
+ *
+ * @param stdClass $args  An object of wp_nav_menu() arguments.
+ * @param WP_Post  $item  Menu item data object.
+ * @param int      $depth Depth of menu item. Used for padding.
+ */
+function twenty_twenty_one_add_menu_description_args( $args, $item, $depth ) {
+	if ( 0 === $depth && isset( $item->description ) && $item->description ) {
+		$args->link_after = '<span class="menu-item-description">' . $item->description . '</span>';
+	}
+	return $args;
+}
+add_filter( 'nav_menu_item_args', 'twenty_twenty_one_add_menu_description_args', 10, 3 );
