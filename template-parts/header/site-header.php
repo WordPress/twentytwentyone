8228abc3ad8c61b5f412f3f44e394049ee494bb2
Ari Stathopoulos <aristath@gmail.com>
2020-10-13 11:15:30 +0300

    Add extra classes to the site-header

--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -7,9 +7,13 @@
  * @since 1.0.0
  */
 
+$wrapper_classes  = 'site-header';
+$wrapper_classes .= has_custom_logo() ? ' has-logo' : '';
+$wrapper_classes .= true === get_theme_mod( 'display_title_and_tagline', true ) ? ' has-title-and-tagline' : '';
+
 ?>
 
-<header id="masthead" class="site-header" role="banner">
+<header id="masthead" class="<?php echo esc_attr( $wrapper_classes ); ?>" role="banner">
 
 	<?php get_template_part( 'template-parts/header/site-branding' ); ?>
 
