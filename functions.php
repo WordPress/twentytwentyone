821aba5778a323294f0355c2ba7c47709c5cfa5c
Carolina Nymark <hi@themesbycarolina.com>
2020-10-13 15:12:23 +0200

    Remove the theme switch from the upgrade notice

--- functions.php
+++ functions.php
@@ -12,7 +12,6 @@
 // This theme requires WordPress 5.3 or later.
 if ( version_compare( $GLOBALS['wp_version'], '5.3', '<' ) ) {
 	require get_template_directory() . '/inc/back-compat.php';
-	return;
 }
 
 if ( ! function_exists( 'twenty_twenty_one_setup' ) ) {
