efe8bdedda0fafef13be3dd797cf419a3d07da81
Simon <bachiebweg@hotmail.com>
2020-10-13 12:01:58 +0200

    Fixes: space in the linked post date, so the link underline looks longer than it should be.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -18,7 +18,7 @@ if ( ! function_exists( 'twenty_twenty_one_posted_on' ) ) {
 	function twenty_twenty_one_posted_on() {
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time> <time class="updated" datetime="%3$s">%4$s</time>';
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 		}
 
 		$time_string = sprintf(
