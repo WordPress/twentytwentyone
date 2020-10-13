ef6e62665655e28e6e619467a7aed1497428eea9
Kelly Dwan <ryelle@users.noreply.github.com>
2020-10-13 17:03:08 -0400

    Post Formats: Check for all embed blocks on audio & video posts

--- template-parts/excerpt/excerpt-audio.php
+++ template-parts/excerpt/excerpt-audio.php
@@ -9,17 +9,13 @@
  * @since 1.0.0
  */
 
-// Print the 1st instance of an audio block.
-// If there was no audioblock, print the 1st audio-embed block.
-if (
-	! twenty_twenty_one_print_first_instance_of_block( 'core/embed', get_the_content() ) &&
-	! twenty_twenty_one_print_first_instance_of_block( 'core/audio', get_the_content() )
-) {
+$content = get_the_content();
 
-	// Fallback to the content.
-	the_content();
+if ( has_block( 'core/audio', $content ) ) {
+	twenty_twenty_one_print_first_instance_of_block( 'core/audio', $content );
 } else {
-
-	// Add the excerpt.
-	the_excerpt();
+	twenty_twenty_one_print_first_instance_of_block( 'core-embed/*', $content );
 }
+
+// Add the excerpt.
+the_excerpt();
--- template-parts/excerpt/excerpt-video.php
+++ template-parts/excerpt/excerpt-video.php
@@ -9,16 +9,13 @@
  * @since 1.0.0
  */
 
-// Print the 1st video we can find.
-if (
-	! twenty_twenty_one_print_first_instance_of_block( 'core/embed', get_the_content() ) &&
-	! twenty_twenty_one_print_first_instance_of_block( 'core/video', get_the_content() )
-) {
+$content = get_the_content();
 
-	// Fallback to the content if no block was found.
-	the_content();
+if ( has_block( 'core/video', $content ) ) {
+	twenty_twenty_one_print_first_instance_of_block( 'core/video', $content );
 } else {
-
-	// Add the excerpt.
-	the_excerpt();
+	twenty_twenty_one_print_first_instance_of_block( 'core-embed/*', $content );
 }
+
+// Add the excerpt.
+the_excerpt();
