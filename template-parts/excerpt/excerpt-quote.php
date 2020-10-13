ad88f30cfb806c9d86cb685781924912226596ac
Kelly Dwan <ryelle@users.noreply.github.com>
2020-10-13 17:25:09 -0400

    Prefer elseif for phpcs

--- template-parts/excerpt/excerpt-quote.php
+++ template-parts/excerpt/excerpt-quote.php
@@ -14,7 +14,7 @@ $content = get_the_content();
 // If there is no quote or pullquote print the content.
 if ( has_block( 'core/quote', $content ) ) {
 	twenty_twenty_one_print_first_instance_of_block( 'core/quote', $content );
-} else if ( has_block( 'core/pullquote', $content ) ) {
+} elseif ( has_block( 'core/pullquote', $content ) ) {
 	twenty_twenty_one_print_first_instance_of_block( 'core/pullquote', $content );
 } else {
 	the_content();
