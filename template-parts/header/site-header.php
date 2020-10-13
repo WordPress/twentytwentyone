613dba81f1ad2de3ad61054709b703137477059c
Ari Stathopoulos <aristath@gmail.com>
2020-10-13 10:39:02 +0300

    split site-header to separate template file

---
+++ template-parts/header/site-header.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Displays the site header.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since 1.0.0
+ */
+
+?>
+
+<header id="masthead" class="site-header" role="banner">
+
+	<?php get_template_part( 'template-parts/header/site-branding' ); ?>
+
+	<?php if ( has_nav_menu( 'primary' ) ) : ?>
+		<nav id="site-navigation" class="primary-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary', 'twentytwentyone' ); ?>">
+			<div class="menu-button-container">
+				<button id="primary-mobile-menu" class="button" aria-controls="primary-menu-list" aria-expanded="false">
+					<span class="dropdown-icon open"><?php esc_html_e( 'Menu', 'twentytwentyone' ); ?>
+						<?php echo twenty_twenty_one_get_icon_svg( 'menu' ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
+					</span>
+					<span class="dropdown-icon close"><?php esc_html_e( 'Close', 'twentytwentyone' ); ?>
+						<?php echo twenty_twenty_one_get_icon_svg( 'close' ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
+					</span>
+				</button><!-- #primary-mobile-menu -->
+			</div><!-- .menu-button-container -->
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location'  => 'primary',
+					'menu_class'      => 'menu-wrapper',
+					'container_class' => 'primary-menu-container',
+					'items_wrap'      => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
+				)
+			);
+			?>
+		</nav><!-- #site-navigation -->
+	<?php endif; ?>
+</header><!-- #masthead -->
