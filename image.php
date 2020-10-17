00022add23cced872f02a7bbb7d0df1f5941c35d
Carolina Nymark <hi@themesbycarolina.com>
2020-10-17 06:42:00 +0200

    Update image.php

--- image.php
+++ image.php
@@ -40,12 +40,10 @@ while ( have_posts() ) {
 
 			wp_link_pages(
 				array(
-					'before'      => '<div class="page-links"><span class="page-links-title">' . esc_html__( 'Pages:', 'twentytwentyone' ) . '</span>',
-					'after'       => '</div>',
-					'link_before' => '<span>',
-					'link_after'  => '</span>',
-					'pagelink'    => '<span class="screen-reader-text">' . esc_html__( 'Page', 'twentytwentyone' ) . ' </span>%',
-					'separator'   => '<span class="screen-reader-text">, </span>',
+					'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
+					'after'    => '</nav>',
+					/* translators: There is a space after page. */
+					'pagelink' => esc_html__( 'Page ', 'twentytwentyone' ) . '%',
 				)
 			);
 			?>
