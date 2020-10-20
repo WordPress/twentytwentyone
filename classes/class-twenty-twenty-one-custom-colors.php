e542243bc4766e86f9bfa6d98e793de2e7c9d335
Jeff Ong <jonger4@gmail.com>
2020-10-20 08:16:12 -0400

    Ignore php stan error.

--- classes/class-twenty-twenty-one-custom-colors.php
+++ classes/class-twenty-twenty-one-custom-colors.php
@@ -194,7 +194,7 @@ class Twenty_Twenty_One_Custom_Colors {
 			$classes[] = 'is-background-light';
 		}
 
-		$should_respect_color_scheme  = get_theme_mod( 'respect_user_color_preference', true );
+		$should_respect_color_scheme  = get_theme_mod( 'respect_user_color_preference', true );  // @phpstan-ignore-line. Passing true instead of default value of false to get_theme_mod.
 		$light_colors_default_palette = array( '#D1E4DD', '#D1DFE4', '#D1D1E4', '#E4D1D1', '#E4DAD1', '#EEEADD', '#FFFFFF' );
 		if ( $should_respect_color_scheme && $this->is_default_palette() ) {
 			$classes[] = 'has-default-light-palette-background';
