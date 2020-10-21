25bb8df8b567a61b97a66e4481327e055062b281
Ari Stathopoulos <aristath@gmail.com>
2020-10-21 08:33:50 +0300

    Add localStorage

--- assets/js/night-day.js
+++ assets/js/night-day.js
@@ -3,15 +3,29 @@ function twentytwentyoneNightDayToggler() {
 		isDarkMode = window.matchMedia( '(prefers-color-scheme: dark)' ).matches,
 		html = document.querySelector( 'html' );
 
+	if ( 'yes' === window.localStorage.getItem( 'twentytwentyoneDarkMode' ) ) {
+		isDarkMode = true;
+	} else if ( 'no' === window.localStorage.getItem( 'twentytwentyoneDarkMode' ) ) {
+		isDarkMode = false;
+	}
+
 	if ( isDarkMode ) {
 		toggler.checked = true;
+	}
+
+	if ( isDarkMode ) {
+		html.classList.add( 'respect-color-scheme-preference' );
+	} else {
+		html.classList.remove( 'respect-color-scheme-preference' );
 	}
 
 	toggler.addEventListener( 'click', function() {
 		if ( toggler.checked ) {
 			html.classList.add( 'respect-color-scheme-preference' );
+			window.localStorage.setItem( 'twentytwentyoneDarkMode', 'yes' );
 		} else {
 			html.classList.remove( 'respect-color-scheme-preference' );
+			window.localStorage.setItem( 'twentytwentyoneDarkMode', 'no' );
 		}
 	} );
 }
