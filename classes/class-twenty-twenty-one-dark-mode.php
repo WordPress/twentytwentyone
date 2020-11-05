db1dfaf8bb33a2d33c5b146964cd1e76abb49de6
Justin Ahinon <justiny.ahinon@gmail.com>
2020-11-05 09:01:56 +0100

    Improve the dark mode option message in the customizer

--- classes/class-twenty-twenty-one-dark-mode.php
+++ classes/class-twenty-twenty-one-dark-mode.php
@@ -164,8 +164,8 @@ class Twenty_Twenty_One_Dark_Mode {
 			array(
 				'type'            => 'checkbox',
 				'section'         => 'colors',
-				'label'           => esc_html__( 'Respect visitor\'s device dark mode settings', 'twentytwentyone' ),
-				'description'     => __( 'Dark mode is a device setting. If a visitor to your site requests it, your site will be shown with a dark background and light text.', 'twentytwentyone' ),
+				'label'           => esc_html__( 'Dark Mode' ),
+				'description'     => __( 'By enabling this option, visitors to your site will be able to switch to Dark Mode. In this mode, the background color of the site will be dark, with light text.', 'twentytwentyone' ),
 				'active_callback' => function( $value ) {
 					return 127 < Twenty_Twenty_One_Custom_Colors::get_relative_luminance_from_hex( get_theme_mod( 'background_color', 'D1E4DD' ) );
 				},
