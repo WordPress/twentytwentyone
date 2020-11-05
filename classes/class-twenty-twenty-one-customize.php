2679369a2b71e6ce85cf0e34d7c11a2bf18243de
Carolina Nymark <myazalea@hotmail.com>
2020-11-05 14:35:51 +0100

    Update class-twenty-twenty-one-customize.php

--- classes/class-twenty-twenty-one-customize.php
+++ classes/class-twenty-twenty-one-customize.php
@@ -108,7 +108,7 @@ if ( ! class_exists( 'Twenty_Twenty_One_Customize' ) ) {
 				array(
 					'type'    => 'radio',
 					'section' => 'excerpt_settings',
-					'label'   => esc_html__( 'On Archive pages, posts show:', 'twentytwentyone' ),
+					'label'   => esc_html__( 'On Archive Pages, posts show:', 'twentytwentyone' ),
 					'choices' => array(
 						'excerpt' => esc_html__( 'Summary', 'twentytwentyone' ),
 						'full'    => esc_html__( 'Full text', 'twentytwentyone' ),
