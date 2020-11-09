023bf4f08f7b480150fd3ed9ec5356d12993c9b2
Ari Stathopoulos <aristath@gmail.com>
2020-11-09 10:02:42 +0200

    missed this in previous commit

--- classes/class-twenty-twenty-one-dark-mode.php
+++ classes/class-twenty-twenty-one-dark-mode.php
@@ -151,8 +151,8 @@ class Twenty_Twenty_One_Dark_Mode {
 
 		$colors_section = $wp_customize->get_section( 'colors' );
 		if ( $colors_section ) {
-			$wp_customize->get_section( 'colors' )->title       = __( 'Colors & Dark Mode', 'twentytwentyone' );
-			$wp_customize->get_section( 'colors' )->description = __( 'To access the Dark Mode settings, select a light background color.', 'twentytwentyone' ) . '<br><a href="https://wordpress.org/support/article/twenty-twenty-one/">' . __( 'Learn more about Dark Mode.', 'twentytwentyone' ) . '</a>';
+			$colors_section->title       = __( 'Colors & Dark Mode', 'twentytwentyone' );
+			$colors_section->description = __( 'To access the Dark Mode settings, select a light background color.', 'twentytwentyone' ) . '<br><a href="https://wordpress.org/support/article/twenty-twenty-one/">' . __( 'Learn more about Dark Mode.', 'twentytwentyone' ) . '</a>';
 		}
 
 		$wp_customize->add_setting(
