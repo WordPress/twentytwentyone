15f73aae2c4b6d0e7f284e3a6d18d5fec539325d
Ari Stathopoulos <aristath@gmail.com>
2020-11-16 17:37:18 +0200

    Remove all break tags and use 2 paragraphs

--- classes/class-twenty-twenty-one-dark-mode.php
+++ classes/class-twenty-twenty-one-dark-mode.php
@@ -182,10 +182,8 @@ class Twenty_Twenty_One_Dark_Mode {
 			)
 		);
 
-		$description  = __( 'Respect visitor&#8217;s device dark mode settings.', 'twentytwentyone' ) . '<br>';
-		$description .= __( 'Dark mode is a device setting. If a visitor to your site requests it, your site will be shown with a dark background and light text.', 'twentytwentyone' ) . '<br>';
-		$description .= '<a href="https://wordpress.org/support/article/twenty-twenty-one/">' . __( 'Learn more about Dark Mode.', 'twentytwentyone' ) . '</a>' . '<br><br>';
-		$description .= __( 'Dark Mode can also be turned on and off with a button that you can find in the bottom right corner of the page.', 'twentytwentyone' ) . '<br>';
+		$description  = '<p>' . __( 'Respect visitor\'s device dark mode settings. Dark mode is a device setting. If a visitor to your site requests it, your site will be shown with a dark background and light text. <a href="https://wordpress.org/support/article/twenty-twenty-one/">Learn more about Dark Mode.</a>', 'twentytwentyone' ) . '</p>';
+		$description .= '<p>' . __( 'Dark Mode can also be turned on and off with a button that you can find in the bottom right corner of the page.', 'twentytwentyone' ) . '</p>';
 
 
 		$wp_customize->add_control(
